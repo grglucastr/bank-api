a48bf24499fed559b4b069d87a4fe47341dfaf4c
vinybergamo <vinybergamo@gmail.com>
2023-05-19 11:18:33 -0300

    chore: configure project bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3333;
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(port, async () =>
+    console.log('Server started on', await app.getUrl()),
+  );
 }
 bootstrap();
